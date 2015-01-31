e25fecf7ae738c0c98db32c6fba7c241b2cbce13
Carla kechichian <cocokechichian@gmail.com>
2015-01-31 12:47:22 -0800

    video 13 , making our character attack .

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -23,6 +23,7 @@ game.PlayerEntity = me.Entity.extend({
 		me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH)
 		this.renderable.addAnimation("idle" , [78]);
 		this.renderable.addAnimation("walk", [117, 118 , 119 , 120 , 121 , 122 , 123 , 124 , 125], 80);
+		this.renderable.addAnimation("attack", [65, 66, 67, 68, 69, 70, 71, 72], 80);
 		this.renderable.setCurrentAnimation("idle");
 	},
 
@@ -40,13 +41,51 @@ game.PlayerEntity = me.Entity.extend({
 			this.body.vel.x = 0; 
 		}
 
-		if(this.body.vel.x !== 0){
+	
+
+if(me.input.isKeyPressed("attack")){
+		
+	if(!this.renderable.isCurrentAnimation("attack")){
+		console.log(!this.renderable.isCurrentAnimation("attack"));
+		//sets the current animation to attack and once that is over
+	//goes back to the idle animation
+	this.renderable.setCurrentAnimation("attack" , "idle");
+	//makes it so that the next time we start this sequence we begin
+	//from the first animation, not wherever we left off when we
+	//switched to another animation
+	this.renderable.setAnimationFrame();
+	}
+}
+
+
+
+
+
+		else if(this.body.vel.x !== 0){
 			if(!this.renderable.isCurrentAnimation("walk")){
 				this.renderable.setCurrentAnimation("walk");
 			}
 		}else{
 			this.renderable.setCurrentAnimation("idle");
 		}
+
+
+if(me.input.isKeyPressed("attack")){
+		
+	if(!this.renderable.isCurrentAnimation("attack")){
+		console.log(!this.renderable.isCurrentAnimation("attack"));
+		//sets the current animation to attack and once that is over
+	//goes back to the idle animation
+	this.renderable.setCurrentAnimation("attack" , "idle");
+	//makes it so that the next time we start this sequence we begin
+	//from the first animation, not wherever we left off when we
+	//switched to another animation
+	this.renderable.setAnimationFrame();
+	}
+}
+
+
+
 		//updating the game
 		this.body.update(delta);
 
