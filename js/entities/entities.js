c7f848ca5a119cd18dc7596bb88a9144508a6131
Carla kechichian <cocokechichian@gmail.com>
2015-02-04 17:52:40 -0800

    video 15 , colliding with bases from left & right , and fixed the jumping problem that the character had

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -20,6 +20,8 @@ game.PlayerEntity = me.Entity.extend({
 		}]);
 		//setting the velocity
 		this.body.setVelocity(5, 20);
+		//keeps track of which direction your character is going
+		this.facing = "right";
 		me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH)
 		this.renderable.addAnimation("idle" , [78]);
 		this.renderable.addAnimation("walk", [117, 118 , 119 , 120 , 121 , 122 , 123 , 124 , 125], 80);
@@ -34,17 +36,19 @@ game.PlayerEntity = me.Entity.extend({
 			//setVelocity() and multiplying it by me.timer.tick
 			//me.timer.tick makes the movement look smooth
 			this.body.vel.x += this.body.accel.x * me.timer.tick;
+
+			this.facing = "right";
 			this.flipX(true);
 
 			//if other key is pressed it wont work
 		}else if (me.input.isKeyPressed("left")){
+			this.facing = "left";
 			this.body.vel.x -=this.body.accel.x * me.timer.tick;
 			this.flipX(false);
 		}else{
 			this.body.vel.x = 0; 
 		}
-
-if(me.input.isKeyPressed("jump") && !this.jumping && !this.falling) {
+if(me.input.isKeyPressed("jump") && !this.body.jumping && !this.body.falling) {
 this.jumping = true;
 this.body.vel.y -= this.body.accel.y * me.timer.tick;
 }
@@ -92,12 +96,30 @@ if(me.input.isKeyPressed("attack")){
 }
 
 
-
+	me.collision.check(this, true, this.collideHandler.bind(this), true);
 		//updating the game
 		this.body.update(delta);
 
 		this._super(me.Entity, "update", [delta]);
 		return true;
+	},
+
+		collideHandler: function(response) {
+		if(response.b.type==='EnemyBaseEntity') {
+			var ydif = this.pos.y - response.b.pos.y;
+			var xdif = this.pos.x - response.b.pos.x;
+			
+			console.log("xdif " + xdif + " ydif " + ydif);
+
+			if(xdif>-35 && this.facing==='right' && (xdif<0)) {
+				this.body.vel.x = 0;
+				this.pos.x = this.pos.x -1;
+			}else if(xdif<70 && this.facing==='left' && xdif>0) {
+				this.body.vel.x = 0;
+				this.pos.x = this.pos.x +1;
+
+			}
+		}
 	}
 });
 
