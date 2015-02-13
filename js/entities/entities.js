89469afd2bd945be0a985f7c06475425fb70a301
Carla kechichian <cocokechichian@gmail.com>
2015-02-13 14:33:43 -0800

    Video 22 , making the creep hit the player, and the player loosing health.

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -18,6 +18,9 @@ game.PlayerEntity = me.Entity.extend({
 				return (new me.Rect (0, 0, 64, 64)).toPolygon(); 
 			}
 		}]);
+		this.type = "PlayerEntity";
+
+		this.health = 20;
 		//setting the velocity
 		this.body.setVelocity(5, 20);
 		//keeps track of which direction your character is going
@@ -100,6 +103,11 @@ if(me.input.isKeyPressed("attack")){
 		//reaches to the constructor of Entity
 		this._super(me.Entity, "update", [delta]);
 		return true;
+	},
+
+	loseHealth: function(damage){
+		this.health = this.health - damage;
+		console.log(this.health);
 	},
 			//sets collision between tower and player it helps so the player wont go through the tower
 		collideHandler: function(response) {
@@ -271,17 +279,40 @@ game.PlayerBaseEntity = me.Entity.extend({
 	collideHandler: function(response){
 		if (response.b.type==='PlayerBase') {
 			this.attacking=true;
-			
+			//this.lastAttacking=this.now;
 			this.body.vel.x= 0;
-			
+			//keeps moving the creep to the right to maintain its position
 			this.pos.x = this.pos.x + 1;
-			
+			//checks that it has been at least 1 second since this creep hit a base
 			if ((this.now-this.lastHit >= 1000 )){
-				
+				//updates the lasthit timer
 				this.lastHit = this.now;
-
+				//makes the player base call its loseHealth function and passes it
+				//a damage of 1
 				response.b.loseHealth(1);
 			}
+		}else if (response.b.type=== 'PlayerEntity'){
+
+			var xdif = this.pos.x - response.b.pos.x;
+
+			this.attacking=true;
+			//this.lastAttacking=this.now;
+			
+			
+			if(xdif>0){
+				//keeps moving the creep to the right to maintain its position
+			this.pos.x = this.pos.x + 1;
+			this.body.vel.x= 0;
+			}	
+			//checks that it has been at least 1 second since this creep hit something
+			if ((this.now-this.lastHit >= 1000) && xdif>0){
+				//updates the lasthit timer
+				this.lastHit = this.now;
+				//makes the player  call its loseHealth function and passes it
+				//a damage of 1
+				response.b.loseHealth(1);
+			}
+
 		}
 	}
 });
