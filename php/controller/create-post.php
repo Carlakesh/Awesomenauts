ecccd25c58347441de369ea7a9005052d2b85ae7
Carla Kechichian <carla.kechichian@hotmail.com>
2015-04-03 12:24:24 -0700

    video 54 , starting php

---
+++ php/controller/create-post.php
@@ -0,0 +1,31 @@
+
+<?php
+
+//calling the right file
+require_once(__DIR__ . "/../model/config.php");
+
+//storing the text into a variable , input is to filter the input making sure no errors with the input.
+// Input post is to send the info to us so we can recieve it.
+//Sanatize string is to make sure no errors are going on
+// the code is to receive the info 
+$title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
+$post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);
+$date = new DateTime("today");
+$time = new DateTime("America/Los_Angeles");
+//see if query runs successfully , establish query to database
+//using single quote on the title to make sure it was being sent, its a string thats being stored
+$query = $_SESSION["connection"]->query("INSERT INTO posts SET title = '$title', post = '$post'");
+//checking if a true value is stored within query 
+//if its true
+if ($query) {
+    echo "<p> Successfully inserted post: $title </p> ";
+    echo "posted on: " . $date->format('M/D/Y') . "at" . $time->format('g:i');
+}
+
+//there is two double quotes that is being used, and thats an issue
+//had to cuncatinate 
+else {
+    echo "<p>" . $_SESSION["connection"]->error . "</p>";
+}
+
+header("Location: " . $path . "index.php");
---
+++ php/model/config.php
@@ -0,0 +1,29 @@
+<?php
+
+//accessing info
+require_once(__DIR__ . "/Database.php");
+require_once(__DIR__ . "/date.php");
+//were creating a new database over and over again, this will help by proserving infos, we wont have to gernorate the info
+session_start();
+session_regenerate_id(true);
+
+// accessing the right path
+$path = "/phpblog/";
+
+// '$' are variables , strings next to it are stored into the variable 
+$host = "localhost";
+$username = "root";
+$password = "root";
+$database = "blog_db";
+
+//checking if the session variable excists . 
+if (!isset($_SESSION["connection"])) {
+    //creating new database object
+    $connection = new Database($host, $username, $password, $database);
+    //session variable,  were signing a value to it after the equal sign
+    $_SESSION["connection"] = $connection;
+}
+
+
+
+
